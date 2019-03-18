d6b9891f0b6b1ce8d5aab4ebeb43ff867da02b26
Marcel Opitz <marcel.opitz@kochzauber.de>
2019-03-18 12:10:37 +0100

    added error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer: false,
+        };
+    }
+
     handleClick(buttonText) {
-        if (buttonText === this.props.quiz_question.answer) {
+        const answerIsCorrect = buttonText === this.props.quiz_question.answer;
+
+        this.setState({incorrectAnswer: !answerIsCorrect});
+
+        if (answerIsCorrect) {
             this.props.showNextQuestionHandler();
         }
     }
@@ -25,6 +36,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right'</p> : null}
             </main>
         );
     }
